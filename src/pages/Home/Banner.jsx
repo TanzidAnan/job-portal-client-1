10fc289f16d60618150a5e203822fc4f584c3444
TanzidAnan <tanzid.anan125@gmail.com>
2025-03-11 07:02:37 +0600

    anamation

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from "motion/react"
-import { easeInOut } from 'motion';
+import { easeInOut, easeOut } from 'motion';
 import teme1 from '../../assets/banner/teme1.jpg'
 import teme2 from '../../assets/banner/teme2.jpg'
 
@@ -10,20 +10,31 @@ const Banner = () => {
             <div className="hero bg-base-200 min-h-96">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className='flex-1'>
-                        <img
+                        <motion.img
+                            animate={{ y: [100, 50, 100] }}
+                            transition={{ duration: 7, repeat: Infinity }}
                             src={teme1}
-                            className="max-w-sm w-64 rounded-lg shadow-2xl" />
+                            className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-500 shadow-2xl" />
+                        <motion.img
+                            animate={{ x: [100, 150, 100] }}
+                            transition={{ duration: 7, repeat: Infinity }}
+                            src={teme2}
+                            className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-500 shadow-2xl" />
                     </div>
                     <div className='flex-1'>
                         <motion.h1
-                            animate={{ x: 50, color: ['#28fc03','#03adfc','#a103fc'] }}
-                            transition={{ duration: 2, delay: 1, ease: easeInOut ,repeat:Infinity}}
+                            animate={{ x: 50, color: ['#28fc03', '#03adfc', '#a103fc'] }}
+                            transition={{ duration: 2, delay: 1, ease: easeInOut, repeat: Infinity }}
                             className="text-5xl font-bold">The Easiest Way
                             to Get Your New Job</motion.h1>
-                        <p className="py-6">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                            quasi. In deleniti eaque aut repudiandae et a id nisi.
-                        </p>
+                        <motion.p
+                        animate={{x:50}}
+                        transition={{repeat:Infinity,ease:easeOut, duration:4,delay:6}}
+                        className="py-6 w-96">
+                            Each month, more than 3 million job seekers turn to
+                            website in their search for work, making over 140,000
+                            applications every single day
+                        </motion.p>
                         <button className="btn btn-primary">Get Started</button>
                     </div>
                 </div>
