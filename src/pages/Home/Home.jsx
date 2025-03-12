bba326ebde7189ea77d2eb4d9ff744d502e83d3a
TanzidAnan <tanzid.anan125@gmail.com>
2025-03-12 06:17:04 +0600

    data load

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner';
 import About from './About';
+import HotJobs from './HotJobs';
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
             <h1>this is home</h1>
             <Banner></Banner>
             <About></About>
+            <HotJobs></HotJobs>
         </div>
     );
 };
