8fdc1a30dcc01738585096a0c7d588bd77a7fac2
TanzidAnan <tanzid.anan125@gmail.com>
2025-03-14 05:11:33 +0600

    form

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -2,34 +2,33 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const JobApply = () => {
-    const {id}=useParams();
+    const { id } = useParams();
     console.log(id);
 
-    const submitJobApplication =(e) =>{
+    const submitJobApplication = (e) => {
         e.preventDefault();
-
+        const form = e.target;
+        const linkin = form.linkin.value
+        const github = form.github.value
+        const resume = form.resume.value
     }
 
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
                 </div>
-                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <div className="card bg-base-100 w-full max-w-full shrink-0 shadow-2xl">
                     <div className="card-body">
-                        <fieldset className="fieldset">
+                        <h1 className="text-5xl font-bold">Apply job and goog Luck now!</h1>
+                        <fieldset onClick={submitJobApplication} className="fieldset">
                             <label className="fieldset-label">LinkeIN URL</label>
-                            <input type="url" name='linkin' className="input" placeholder="LinkeIn" />
+                            <input type="url" name='linkin' className="input w-full" placeholder="LinkeIn" />
                             <label className="fieldset-label">GitHub URL</label>
-                            <input type="url" name='github' className="input" placeholder="GitHub URL" />
+                            <input type="url" name='github' className="input w-full" placeholder="GitHub URL" />
                             <label className="fieldset-label">Resume</label>
-                            <input type="url" name='resume' className="input" placeholder="Resume URL" />
-                            <button className="btn btn-neutral mt-4">Apply</button>
+                            <input type="url" name='resume' className="input w-full" placeholder="Resume URL" />
+                            <button className="btn bg-purple-500 mt-4">Apply</button>
                         </fieldset>
                     </div>
                 </div>
