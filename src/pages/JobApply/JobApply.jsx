e1cea3ec8a2882686c10821d3f39a987a13f1402
TanzidAnan <tanzid.anan125@gmail.com>
2025-03-14 06:20:40 +0600

    data added of mongodb

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,36 +1,82 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import useAuth from '../../Hooks/UseAuth';
+import Swal from 'sweetalert2';
 
 const JobApply = () => {
     const { id } = useParams();
+    const { user } = useAuth();
+    console.log(user)
     console.log(id);
 
     const submitJobApplication = (e) => {
         e.preventDefault();
         const form = e.target;
-        const linkin = form.linkin.value
-        const github = form.github.value
-        const resume = form.resume.value
+        const linkin =form.linkin.value;
+        const github = form?.github?.value;
+        const resume = form?.resume?.value;
+        console.log(linkin, github, resume)
+
+        const jobApplication = {
+            job_id: id,
+            applicant_email: user.email,
+            linkin,
+            github,
+            resume
+        }
+        fetch('http://localhost:5000/job-application', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(jobApplication)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if(data.insertedId){
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Your work has been saved",
+                        showConfirmButton: false,
+                        timer: 1500
+                      });
+                }
+                console.log(data)
+            })
+
     }
 
+
+
     return (
-        <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                </div>
-                <div className="card bg-base-100 w-full max-w-full shrink-0 shadow-2xl">
-                    <div className="card-body">
-                        <h1 className="text-5xl font-bold">Apply job and goog Luck now!</h1>
-                        <fieldset onClick={submitJobApplication} className="fieldset">
-                            <label className="fieldset-label">LinkeIN URL</label>
-                            <input type="url" name='linkin' className="input w-full" placeholder="LinkeIn" />
-                            <label className="fieldset-label">GitHub URL</label>
-                            <input type="url" name='github' className="input w-full" placeholder="GitHub URL" />
-                            <label className="fieldset-label">Resume</label>
-                            <input type="url" name='resume' className="input w-full" placeholder="Resume URL" />
-                            <button className="btn bg-purple-500 mt-4">Apply</button>
-                        </fieldset>
-                    </div>
+        <div className="hero bg-base-200 min-h-[70vh]">
+            <div className=" flex-col lg:flex-row-reverse">
+                <div className="card bg-base-100 w-full max-w-4xl shrink-0 shadow-2xl">
+                <h1 className="text-5xl font-bold">job apply  now!</h1>
+                    <form onSubmit={submitJobApplication} className="card-body">
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">linkin URL</span>
+                            </label>
+                            <input type="url" name='linkin' placeholder="linkin url" className="input input-bordered w-full" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">github URL</span>
+                            </label>
+                            <input type="url" name='github' placeholder="github url" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">resume</span>
+                            </label>
+                            <input type="url" name='resume' placeholder="resume" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Job Apply</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
