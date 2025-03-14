bf7b3f5ef3230642255a9ee19608cdfc2541ba4e
TanzidAnan <tanzid.anan125@gmail.com>
2025-03-14 08:48:41 +0600

    table

--- src/pages/MyApplication/MyApplication.jsx
+++ src/pages/MyApplication/MyApplication.jsx
@@ -1,21 +1,75 @@
-import React, { useEffect, useState } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import useAuth from '../../Hooks/UseAuth';
 
 const MyApplication = () => {
-    const {user} =useAuth();
-    const [jobs,setJobs] =useState([]);
-   
+    const { user } = useAuth();
+    const [jobs, setJobs] = useState([]);
+
     console.log(user.email)
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch(`http://localhost:5000/job-application?email=${user.email}`)
-        .then(res => res.json())
-        .then(data =>setJobs(data))
-    } ,[user.email])
+            .then(res => res.json())
+            .then(data => setJobs(data))
+    }, [user.email])
 
     return (
         <div>
             <h1>MY application {jobs.length}</h1>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            jobs.map(job => <tr key={job._id}>
+                                <th>
+                                    <label>
+                                        <input type="checkbox" className="checkbox" />
+                                    </label>
+                                </th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle h-12 w-12">
+                                                <img
+                                                    src={job.company_logo}
+                                                    alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">{job.title}</div>
+                                            <div className="text-sm opacity-50">United States</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    {job.title}
+                                    <br />
+                                    <span className="badge badge-ghost badge-sm">{job.location}</span>
+                                </td>
+                                <td>Purple</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-xs bg-red-700">x</button>
+                                </th>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
