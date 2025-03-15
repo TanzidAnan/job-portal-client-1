7eedf27bb46b2c4f9029afffe2b07b52997dde9c
TanzidAnan <tanzid.anan125@gmail.com>
2025-03-15 07:02:31 +0600

    salary range

--- src/pages/AddJobs/AddJobs.jsx
+++ src/pages/AddJobs/AddJobs.jsx
@@ -19,11 +19,54 @@ const AddJobs = () => {
                     </label>
                     <input type="text" name='location' placeholder="job Location" className="input input-bordered" required />
                 </div>
+                {/* job type */}
                 <div className="form-control">
                     <label className="label">
-                        <span className="label-text">Password</span>
+                        <span className="label-text">job type</span>
                     </label>
-                    <input type="password" placeholder="password" className="input input-bordered" required />
+                    <select className="select select-ghost w-full max-w-xs">
+                        <option disabled selected>Pick a job type</option>
+                        <option>Full Time</option>
+                        <option>pat Time</option>
+                        <option>Intain</option>
+                    </select>
+                </div>
+                {/* job type */}
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">job Field</span>
+                    </label>
+                    <select className="select select-ghost w-full max-w-xs">
+                        <option disabled selected>Pick a job Field</option>
+                        <option>Engineering</option>
+                        <option>Markting</option>
+                        <option>teaching</option>
+                    </select>
+                </div>
+                {/* job company */}
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">company</span>
+                    </label>
+                    <input type="text" name='company' placeholder="job company" className="input input-bordered" required />
+                </div>
+                {/* salary Raing */}
+                <p>Salary Range</p>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'> 
+                    <div className="form-control">
+                        <input type="number" name='min' placeholder="min Number" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <input type="number" name='max' placeholder="Max Number" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <select className="select select-ghost w-full max-w-xs">
+                            <option disabled selected>currency</option>
+                            <option>BDT</option>
+                            <option>USD</option>
+                            <option>INR</option>
+                        </select>
+                    </div>
                 </div>
                 <div className="form-control mt-6">
                     <button className="btn btn-primary">Login</button>
