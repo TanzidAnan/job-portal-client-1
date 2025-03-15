e53e136baf623fa6268cec1b2ea8c3d125c95b6e
TanzidAnan <tanzid.anan125@gmail.com>
2025-03-15 06:41:26 +0600

    add jobs

--- src/pages/AddJobs/AddJobs.jsx
+++ src/pages/AddJobs/AddJobs.jsx
@@ -4,6 +4,31 @@ const AddJobs = () => {
     return (
         <div>
             <h2>Post A new Jobs</h2>
+            <form className="card-body">
+                {/* job title */}
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">job Title</span>
+                    </label>
+                    <input type="text" name='title' placeholder="job Title" className="input input-bordered" required />
+                </div>
+                {/* job location */}
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Location</span>
+                    </label>
+                    <input type="text" name='location' placeholder="job Location" className="input input-bordered" required />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type="password" placeholder="password" className="input input-bordered" required />
+                </div>
+                <div className="form-control mt-6">
+                    <button className="btn btn-primary">Login</button>
+                </div>
+            </form>
         </div>
     );
 };
