09baa1440a382d9285246f749ed055ec386df3f9
TanzidAnan <tanzid.anan125@gmail.com>
2025-03-16 20:51:25 +0600

    data

--- src/pages/MyPostedJobs/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs/MyPostedJobs.jsx
@@ -3,21 +3,44 @@ import useAuth from '../../Hooks/UseAuth';
 
 const MyPostedJobs = () => {
 
-    const [jobs,setJobs] =useState([]);
-    const {user} =useAuth();
+    const [jobs, setJobs] = useState([]);
+    const { user } = useAuth();
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch(`http://localhost:5000/jobs?email=${user.email}`)
-        .then(res=> res.json())
-        .then(data =>{
-            console.log(data);
-            setJobs(data)
-        })
-    },[user.email])
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setJobs(data)
+            })
+    }, [user.email])
 
     return (
         <div>
             <h1>MY Posted Jobs {jobs.length}</h1>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            jobs.map((job,index) => <tr className="bg-base-200">
+                                <th>{index+1}</th>
+                                <td>Cy Ganderton</td>
+                                <td>Quality Control Specialist</td>
+                                <td>Blue</td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
