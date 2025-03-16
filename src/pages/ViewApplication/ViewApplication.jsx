c91ef39af66106f75ade6bcfc725b350c61913c9
TanzidAnan <tanzid.anan125@gmail.com>
2025-03-16 22:21:17 +0600

    table

--- src/pages/ViewApplication/ViewApplication.jsx
+++ src/pages/ViewApplication/ViewApplication.jsx
@@ -3,11 +3,36 @@ import { useLoaderData } from 'react-router-dom';
 
 const ViewApplication = () => {
 
-    const application =useLoaderData();
+    const application = useLoaderData();
 
     return (
         <div>
             <h2>Application for this Job {application.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Email</th>
+                            <th>Email</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            application.map((app,index) => <tr key={app._id} className="bg-base-200">
+                                <th>{index+1}</th>
+                                <td>{app.applicant_email}</td>
+                                <td>Quality Control Specialist</td>
+                                <td>Blue</td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
