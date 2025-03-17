99bdc6983e16246e5e83ad73290f343c96b392ca
TanzidAnan <tanzid.anan125@gmail.com>
2025-03-18 05:47:27 +0600

    view jobs

--- src/pages/ViewApplication/ViewApplication.jsx
+++ src/pages/ViewApplication/ViewApplication.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const ViewApplication = () => {
 
+    const neveget =useNavigate();
+
     const application = useLoaderData();
 
-    const hendleStatusUpdate =(e,id) =>{
-        console.log(e.target.value, id)
+    const hendleStatusUpdate = (e, id) => {
+        console.log(e.target.value, id);
+        const data = {
+            status: e.target.value
+        }
+        fetch(`http://localhost:5000/job-application/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Your work has been saved",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                    neveget('/myPostedJobs')
+                }
+            })
     }
 
     return (
@@ -19,7 +45,7 @@ const ViewApplication = () => {
                         <tr>
                             <th></th>
                             <th>Email</th>
-                            <th>States</th>
+                            <th>status</th>
                             <th>Update States</th>
                         </tr>
                     </thead>
@@ -29,10 +55,10 @@ const ViewApplication = () => {
                             application.map((app, index) => <tr key={app._id} className="bg-base-200">
                                 <th>{index + 1}</th>
                                 <td>{app.applicant_email}</td>
-                                <td>Status</td>
+                                <td>status</td>
                                 <td>
                                     <select
-                                        onChange={(e)=> hendleStatusUpdate(e,app._id)}
+                                        onChange={(e) => hendleStatusUpdate(e, app._id)}
                                         defaultValue={app.States || 'Change Status'}
                                         className="select select-secondary">
                                         <option disabled={true}>Change Status</option>
