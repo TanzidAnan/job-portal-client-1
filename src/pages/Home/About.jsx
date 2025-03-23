d4de86c63764caf51c427ab4c8753c471dcfbf03
TanzidAnan <tanzid.anan125@gmail.com>
2025-03-23 23:59:45 +0600

    about

--- src/pages/Home/About.jsx
+++ src/pages/Home/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
                 <motion.h1
                 animate={{color:['#58eb34','#2aeb64','#2aebbe','#2aebeb']}}
                 transition={{repeat:Infinity,duration:2,delay:1,ease:"easeInOut"}}
-                className="text-4xl font-semibold">Browse by category</motion.h1>
+                className="text-4xl font-semibold">Browse the by category</motion.h1>
                 <p className="mt-5">Find the job that’s perfect for you. about 800+ new jobs everyday</p>
             </div>
         </div>
